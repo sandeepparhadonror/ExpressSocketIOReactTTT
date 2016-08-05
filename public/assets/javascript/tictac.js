07b39e7852e74c19764b96f78f610623004eb531
sandyparhad <sandeepparh@cybage.com>
2016-08-05 15:33:41 +0530

    tic toy game form name done

--- public/assets/javascript/tictac.js
+++ public/assets/javascript/tictac.js
@@ -1,32 +1,63 @@
 var Cells = React.createClass({
   render: function() {
+    var myFontSize = parseInt((10 / this.props.size)*2, 10) + 'em';
+    var myStyle = {
+        'fontSize': myFontSize
+    };
+
     return (
-          <td>{this.props.data}</td>
+      <td onClick={this._onClick} style={myStyle} width={100 / this.props.size + '%'} height={100 / this.props.size + '%'}>
+          {this.props.data}
+      </td>
     );
+  },
+
+  _onClick: function () {
+      if (typeof this.props.onTurnPlayed === 'function') {
+          this.props.onTurnPlayed(this.props.turn, this.props.rowNum, this.props.colNum);
+      }
   }
+
 });
 
 var Rows = React.createClass({
   render: function() {
-    var i, cols = [];
+    var i, cells = [];
     for (i = 0; i < this.props.size; i++){
-      cols.push(<Cells {...this.props} key={i} data={this.props.data[i]} />);
+      cells.push(<Cells {...this.props} key={i} colNum={i} data={this.props.data[i]} />);
     }
 
     return (
         <tr>
-          {cols}
+          {cells}
         </tr>
     );
   }
 });
 
 var TicTacTable = React.createClass({
+
+  propTypes: {
+      size: React.PropTypes.number.isRequired,
+      data: React.PropTypes.array.isRequired,
+      onTurnPlayed: React.PropTypes.func,
+      onRestart: React.PropTypes.func
+  },
+
+
+  getDefaultProps: function () {
+      return {
+          markerOne: 'X',
+          markerTwo: 'O'
+      };
+  },
+
   render: function(){
     var i, rows = [];
     for (i = 0; i < this.props.size; i++){
-      rows.push(<Rows {...this.props} key={i} data={this.props.data[i]}/>);
+      rows.push(<Rows {...this.props}  data={this.getMappedData(this.props.data[i])} key={i} rowNum={i}/>);
     }
+  //  var winner = this.props.winnerData.winner;
     return (
       <div>
         <div id="tic-toy-game-page" className="row page">
@@ -38,7 +69,26 @@ var TicTacTable = React.createClass({
         </div>
       </div>
     );
+  },
+
+  _restartGame: function () {
+      if (typeof this.props.onRestart === 'function') {
+          this.props.onRestart();
+      }
+  },
+
+  getMappedData: function (data) {
+      var hash = {
+          0: null,
+          1: this.props.markerOne,
+          2: this.props.markerTwo
+      };
+
+      return data.map(function (data) {
+          return hash[data];
+      });
   }
+
 });
 
 var MainApp = React.createClass({
@@ -54,19 +104,23 @@ var MainApp = React.createClass({
     },
 
     componentWillUnmount: function() {
-
+        this.socket.disconnect();
     },
 
     render: function () {
         return (
+          /*  <div className="ticTacToeBoard">
+               <h2> Tic Tac Toe </h2> */
 
-            <div className="ticTacToeBoard">
+               <TicTacTable   size={this.state.size}
+                              winnerData={this.state.winnerData}
+                              turn={2}
+                              data={this.state.date}
+                              onTurnPlayed={this.state._onTurnPlayed}
+                              onRestart={this.state._onRestartGame}/>
 
-                <h2> Tic Tac Toe </h2>
 
-                <TicTacTable size={SIZE} data={data}/>
-
-                <div id="enter-page" className="row page">
+/*                <div id="enter-page" className="row page">
                   <div className="col-md-12">
                     <hr></hr>
                     <div className="form-group">
@@ -74,7 +128,7 @@ var MainApp = React.createClass({
                     </div>
                     <div className="control-group">
                       <div className="controls">
-                        <button id="playBtn" className="btn btn-success" onClick={this._onclick}>Play</button>
+                        <button id="playBtn" className="btn btn-success" onClick={this._getPlayerName}>Play</button>
                       </div>
                     </div>
                   </div>
@@ -105,15 +159,30 @@ var MainApp = React.createClass({
                   </div>
                 </div>
 
-            </div>
+                <div id="restart-game">
+                  <div id="popupMessage">You win Gane</div>
+                  <button id="restartGame" className="restartGame" onClick={this._restartGame}>Restart Game</button>
+                </div> */
+
+
+          /*  </div> */
         );
     },
 
 
-    _onclick: function() {
+    _getPlayerName: function() {
       console.log("we have send player name start game")
-      var playerName = setPlayerName();
+      var playerName = this.setPlayerName();
+      this.socket.emit('game:start', playerName);
 
+    },
+
+    _onTurnPlayed: function (turn, rowNum, colNum) {
+        this.socket.emit('game:play:turn', this.state.marker, rowNum, colNum);
+    },
+
+    _onRestartGame: function(){
+        this.socket.emit('game:restart');
     },
 
     setPlayerName: function() {
@@ -126,14 +195,79 @@ var MainApp = React.createClass({
     },
 
 
+
+    _looserDetail: function(looser, looser_playerName) {
+        'looser_playerName is lost the Game'
+    },
+
+    _winnerDetail: function(winner, winner_playerName) {
+     'winner_playerName is win the Game'
+    },
+
+
     _hideOtherComponet: function() {
         document.getElementById('waiting-page').style.display = "none";
         document.getElementById('result-page').style.display = "none";
         document.getElementById('room-size-full').style.display = "none";
         document.getElementById('tic-toy-game-page').style.display = "none";
+        document.getElementById('restart-game').style.display = 'none';
     },
 
     _bindEvents: function(){
+      var self = this;
+      var playerMarker;
+      var playerMarker_name;
+
+        this.socket.on('game:started', function(options){
+
+          self.setState({
+            data: options.data,
+            size: options.size || self.state.size,
+            marker: options.marker || self.state.marker,
+            player_marker_name: options.player_marker_name || self.state.player_marker_name
+          });
+
+
+        });
+
+        this.socket.on('game:start:ready:to:play', function(){
+            console.log("game:start:ready:to:play");
+            document.getElementById('enter-page').style.display = "none";
+            document.getElementById('waiting-page').style.display = "none";
+            document.getElementById('tic-toy-game-page').style.display = "block";
+        });
+
+        this.socket.on('game:started:other:layer:onhold', function(){
+            console.log("game:started:other:layer:onhold");
+            document.getElementById('waiting-page').style.display = "block";
+            document.getElementById('enter-page').style.display = "none";
+            document.getElementById('tic-toy-game-page').style.display = "block";
+        });
+
+        this.socket.on('game:start:limit:exceeded', function(){
+            document.getElementById('enter-page').style.display = "none";
+            document.getElementById('room-size-full').style.display = "block";
+            document.getElementById('waiting-page').style.display = "none";
+            document.getElementById('result-page').style.display = "none";
+            document.getElementById('tic-toy-game-page').style.display = "none";
+            document.getElementById('enter-page').style.display = "none";
+        });
+
+
+
+        this.socket.on('game:result', function(looser, winner ,looser_playerName, winner_playerName){
+            console.log("game result called");
+            var gameResult = playerMarker === looser ? 'You lost the Game' : 'You won the Game';
+
+            document.getElementById('restart-game').style.display = 'block';
+            document.getElementById('popupMessage').innerHTML = gameResult;
+            document.getElementById('enter-page').style.display = "none";
+        });
+
+        this.socket.on('game:result:done', function(){
+              console.log("Restart the new Game");
+              window.location.reload();
+        });
 
 
     }
@@ -141,21 +275,21 @@ var MainApp = React.createClass({
 });
 
 // Dynamic table creation
-var SIZE = 4;
-
-function getDataArray(size) {
-    var i, j, rowArr = [], colArr = [];
-    for(i = 0; i < size; i++) {
-        for(j = 0; j < size; j++) {
-            colArr[j] = 0;
-        }
-        rowArr[i] = colArr;
-    }
-    return rowArr;
-};
-
-var data = getDataArray(SIZE);
-
+// var SIZE = 4;
+//
+// function getDataArray(size) {
+//     var i, j, rowArr = [], colArr = [];
+//     for(i = 0; i < size; i++) {
+//         for(j = 0; j < size; j++) {
+//             colArr[j] = 0;
+//         }
+//         rowArr[i] = colArr;
+//     }
+//     return rowArr;
+// };
+//
+// var data = getDataArray(SIZE);
+//
 // React main componet calling
 
 ReactDOM.render(
--- server.js
+++ server.js
@@ -24,58 +24,106 @@ app.use(function(req, res, next) {
 });
 
 
-/* Game configs */
+/* Game variable configs */
+var SIZE = 5,
+    MARKERS = ['X', 'O'],
+    current_player_connection = 0,
+    players = [],
+    initialGameState, activeMarker;
 
-var current_player_connection = 0;
+function _init() {
+    initialGameState = getInitialState(SIZE);
+    activeMarker = MARKERS[0];
+    activePlayerMarkerName = players[0];
+}
 
+function getInitialState(size) {
+    var i, j, result = [], row;
+    for (i = 0; i < size; i++) {
+        row = [];
+        for (j = 0; j < size; j++) {
+            row.push(0);
+        }
+        result.push(row);
+    }
+    return result;
+};
+
+/*  Kick off app state */
+
+_init();
 
 io.on('connection', function(socket){
-
-    var players
+    ++current_player_connection;
 
     socket.on('game:start', function(playerName){
-        ++current_player_connection;
 
-        console.log(playerName);
-        console.log(++current_player_connection);
+        socket.playerName = playerName;
+        players.push( playerName );
+
+
+        socket.marker = MARKERS[current_player_connection - 1];
+        socket.player_name = players[current_player_connection - 1]
 
 
         if (current_player_connection > 2) {
-
+          console.log("3rd player connecting");
+          if (players.length > 2){
+            players.pop();
+          }
+          socket.emit('game:start:limit:exceeded');
+          return;
         }
+
+        socket.emit('game:started', {
+          size: SIZE,
+          data: initialGameState,
+          marker: MARKERS[current_player_connection - 1],
+          player_marker_name: players[current_player_connection - 1],
+          activeMarker: activeMarker,
+          activePlayerMarkerName: activePlayerMarkerName
+        });
 
         if (current_player_connection < 2) {
-          socket.playerName = playerName
-          players.push({
-            playerName : playerName
-          })
-
+          console.log("first player is join"+ players[0]);
+          socket.emit('game:started:other:layer:onhold');
+        }else {
+          io.emit('game:start:ready:to:play',{});
         }
 
-
-
-
-     socket.playerName = playerName;
-     players.push({
-       playerName : playerName,
-       socketID : socket.id
-     })
-
-     console.log(players.length);
-
-     if (players.length === 2 ){
-         socket.emit('joined game', game);
-     }
-     else if (players.length > 2 ) {
-      // socket.emit('joined game', game);
-      console.log("game book");
-      socket.emit('gameFull', playerName);
-     }
-     else {
-      console.log('Some Thing went wrong');
-    }
-
+        console.log("The number of player is " + players + "in Game" );
+        console.log("The " + current_player_connection + " st player joined the ganme");
     //  io.emit('add player', playerName);
    });
+
+   socket.on('game:restart', function(){
+      io.emit('game:result:done', {});
+   });
+
+   socket.on('disconnect', function(){
+     console.log(--current_player_connection);
+     if (current_player_connection === 0){
+       console.log("All player left the Game");
+       _init();
+     } else if (current_player_connection < 2) {
+
+       var looser = socket.marker;
+       var looser_playerName = socket.playerName
+       var winner_playerName = getWinnerPlayerName(socket.playerName);
+       var winner = getOpponentMarker(socket.marker);
+       console.log("Player " + looser + " and name is" + looser_playerName + " left the game");
+       console.log("Player " + winner + "and winner anem is" + winner_playerName +" the game");
+       io.emit('game:result', looser, winner ,looser_playerName, winner_playerName );
+     }
+   });
+
+   function getWinnerPlayerName(winner_player_name) {
+     return winner_player_name ===  players[0] ? players[1] : players[0];
+   }
+
+   function getOpponentMarker(marker) {
+       return marker === MARKERS[0] ? MARKERS[1] : MARKERS[0];
+   }
+
 
  });
